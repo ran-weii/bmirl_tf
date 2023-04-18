0887c76076729e7542f60203d1034204ec1a9edc
HFML <rw422@tamu.edu>
2023-04-17 20:27:43 -0500

    commit updated birl and rirl configs, halfcheetah config is different from rest

--- examples/config/birl/base.py
+++ examples/config/birl/base.py
@@ -1,9 +1,9 @@
 base_params = {
-    'type': 'RIRL',
+    'type': 'BIRL',
     'universe': 'gym',
 
     'kwargs': {
-        'log_dir': './logs/rirl',
+        'log_dir': './logs/birl',
         'log_wandb': False,
 
         'epoch_length': 1000,
@@ -19,7 +19,6 @@ base_params = {
         # sac args
         'discount': 0.99,
         'tau': 5e-3,
-        'reward_scale': 1.0,
         'alpha': 1.,
         'min_alpha': 0.001,
         'auto_alpha': True,
@@ -27,34 +26,40 @@ base_params = {
         # reward args
         'num_expert_traj': 100,
         'rwd_done_flag': True,
-        'rwd_clip_max': 10,
-        'rwd_rollout_batch_size': 64,
-        'rwd_rollout_length': 100,
-        'rwd_update_method': 'traj', # traj or marginal
+        'reward_scale': 1.0,
+        'rwd_clip_max': 10.,
+        'rwd_rollout_batch_size': 1000,
         'rwd_update_steps': 1,
-        'grad_penalty': 1.,
+        'rwd_rollout_length': 40,
+        'rwd_update_method': 'traj', # traj or marginal
+        'grad_penalty': 4.,
         'l2_penalty': 0.001,
 
-        # 'num_expert_traj': 50,
-        # 'rwd_done_flag': True,
-        # 'rwd_clip_max': 10,
-        # 'rwd_rollout_batch_size': 64,
-        # 'rwd_rollout_length': 100,
-        # 'rwd_update_method': 'marginal', # traj or marginal
-        # 'rwd_steps': 50,
-        # 'grad_penalty': 1,
-        
         # dynamics args
         'adversary_loss_weighting': 0.0768,
         'adv_update_steps': 50,
         'adv_rollout_length': 10,
-        
+
+        # 'num_expert_traj': 100,
+        # 'rwd_done_flag': True,
+        # 'reward_scale': 1.0,
+        # 'rwd_clip_max': 40.,
+        # 'rwd_rollout_batch_size': 1000,
+        # 'rwd_update_steps': 40,
+        # 'rwd_rollout_length': 40,
+        # 'rwd_update_method': 'marginal', # traj or marginal
+        # 'grad_penalty': 4,
+
+        # 'adversary_loss_weighting': 0.0768,
+        # 'adv_update_steps': 40,
+        # 'adv_rollout_length': 10,
+    
         # training args
         'critic_lr': 3e-4,
         'actor_lr': 1e-4,
         'adv_lr': 1e-4,
         'rwd_lr': 1e-4,
-        'real_ratio': 0.5,
+        'real_ratio': 0.01,
         'train_adversarial': True,
         'model_rollout_freq': 250,
         'model_retain_epochs': 5,
@@ -63,7 +68,7 @@ base_params = {
         'deterministic': False,
         'num_networks': 7,
         'num_elites': 5,
-        
+
         # pretrain args
         'max_model_t': None,
         'pretrain_bc': False,
